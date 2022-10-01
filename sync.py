cc7747dc7026f18b73fa9ee6db247bd0e07ccbb0
mavhungutrezzy <mavhungutrezzy@gmail.com>
2022-10-01 18:57:57 +0200

    refactor - reduce code complexity

--- sync.py
+++ sync.py
@@ -9,27 +9,23 @@ def parse_markdown_to_html_table():
     readme = open("README.md", 'r').read()
     html = markdown2.markdown(readme, extras=['tables'])
     soup = BeautifulSoup(html, 'html.parser')
-    table = soup.find("table")
-    return table
+    return soup.find("table")
 
 def parse_html_table(table):
     """ Parse HTML table into proper formatted array that can be written to Google Sheet."""
     result = []
     rows = table.findAll('tr')
     for row in rows:
-        current_row = []
         cols = row.findAll(['td', 'th'])
-        for cell in cols:
-            current_row.append(html_to_spreadsheet_cell(cell))
+        current_row = [html_to_spreadsheet_cell(cell) for cell in cols]
         result.append(current_row)
-    filler_rows = [["", "", "", ""] for x in range(10)]
+    filler_rows = [["", "", "", ""] for _ in range(10)]
     return result + filler_rows
 
 def html_to_spreadsheet_cell(html_element):
     """ Parse HTML element, like <a href=www.google.com>Google</a> to =HYPERLINK(www.google.com, Google) """
-    link = html_element.find("a")
-    if link:
-        return '=HYPERLINK("{}", "{}")'.format(link['href'], link.contents[0])
+    if link := html_element.find("a"):
+        return f"""=HYPERLINK("{link['href']}", "{link.contents[0]}")"""
     else:
         return html_element.text
 
