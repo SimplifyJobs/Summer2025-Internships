a52681f3646ce3bbd06a2a13f1fc73acc49dd73d
Matt Reed <matthewreed02@gmail.com>
2023-06-27 15:24:16 -0700

    fixing actions issues

--- simplify/log.py
+++ simplify/log.py
@@ -5,7 +5,7 @@ import os
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger_file_handler = logging.handlers.RotatingFileHandler(
-    "status.log",
+    "simplify/status.log",
     maxBytes=1024 * 1024,
     backupCount=1,
     encoding="utf8",
