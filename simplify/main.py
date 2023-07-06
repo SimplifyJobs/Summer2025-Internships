833cf648b8305447c89d5fcdc4b4c007c60331e8
Matt Reed <matthewreed02@gmail.com>
2023-07-06 15:40:45 -0400

    splits listings into multiple for each listing

--- simplify/main.py
+++ simplify/main.py
@@ -7,8 +7,10 @@ from datetime import date
 
 
 def create_md_table(listings):
-    col_names = ["Name", "Location", "Notes", "Date Posted", "Open"]
-    col_keys = ["Name", "Location", "Notes", "date_posted"]
+    col_names = ["Company", "Company Bio",
+                 "Location", "Title", "Date Posted", "Open"]
+    col_keys = ["name", "company_desciption",
+                "location", "title", "date_posted"]
     result = "| "
     for name in col_names:
         result += name + " | "
@@ -18,12 +20,33 @@ def create_md_table(listings):
     for listing in listings:
         result += "\n| "
         for key in col_keys:
-            result += listing[key] + " | "
+            link = listing["site_link"]
+            if key == "title":
+                result += "[" + listing[key] + "](" + link + ") | "
+            else:
+                result += listing[key] + " | "
         if not listing["is_closed"]:
             result += "✅ |"
         else:
             result += "🚫 |"
     return result
+
+
+def remove_links(string: str):
+    links = {}
+    while True:
+        bracket_open = string.find("[")
+        bracket_closed = string.find("]", bracket_open)
+        paren_open = string.find("(", bracket_closed)
+        paren_closed = string.find(")", paren_open)
+        if (bracket_open == -1 or bracket_closed == -1 or paren_open == -1 or paren_closed == -1):
+            break
+        text = string[bracket_open+1:bracket_closed]
+        link = string[paren_open+1:paren_closed]
+        links[text] = link
+        string = string[:bracket_open] + text + string[paren_closed+1:]
+
+    return string, links
 
 
 def parse_file(listings: list, path: str, year: int, is_closed: bool, is_off_season: bool):
@@ -36,10 +59,12 @@ def parse_file(listings: list, path: str, year: int, is_closed: bool, is_off_sea
                 if table_line_num == 0:
                     header = [t.strip(" ") for t in line.split('|')[1:-1]]
                 elif table_line_num > 1:
-                    values = [t.strip() for t in line.split('|')[1:-1]]
+                    stripped_line, links = remove_links(line)
+                    values = [t.strip()
+                              for t in stripped_line.split('|')[1:-1]]
                     job_is_closed = False
                     for col, value in zip(header, values):
-                        data[col] = value
+                        data[col.lower()] = value
                         if value.find("Closed") != -1:
                             job_is_closed = True
                             start = value.find("**")
@@ -48,9 +73,17 @@ def parse_file(listings: list, path: str, year: int, is_closed: bool, is_off_sea
                     data["is_closed"] = is_closed or job_is_closed
                     data["year"] = year
                     data["is_off_season"] = is_off_season
-                    listings.append(data)
+                    data["company_desciption"] = "Not Implemented"
                     if "date_posted" not in data:
-                        data["date_posted"] = date.today().strftime("%d %b %Y")
+                        data["date_posted"] = date.today().strftime("%b %d, %Y")
+                    for text, link in links.items():
+                        if len(links) == 1 or text != values[0]:
+                            listing = dict(data)
+                            listing["site_link"] = link
+                            listing["title"] = text if len(
+                                links) > 1 else listing["notes"]
+                            listings.append(listing)
+
                 table_line_num += 1
 
 
@@ -64,9 +97,11 @@ def main():
     n = text_file.write(json.dumps(result, indent=4))
     text_file.close()
     text_file = open("TABLE.md", "w")
-    n = text_file.write(create_md_table(result))
+    summer_listings = list(filter(
+        lambda x: not x["is_closed"] and not x["is_off_season"] and x["year"] == 2024, result))
+    n = text_file.write(create_md_table(summer_listings))
     text_file.close()
-    logger.info(f"dumped json file with {len(result)} job listings")
+    logger.info(f"dumped json file with {len(summer_listings)} job listings")
 
 
 if __name__ == "__main__":
