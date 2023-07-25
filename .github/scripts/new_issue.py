c98cdde0a2ec4b15c2411bd56f73bfdb7452f8b6
Matt Reed <matthewreed02@gmail.com>
2023-07-25 15:22:55 -0400

    aergaerfaeg

--- .github/scripts/new_issue.py
+++ .github/scripts/new_issue.py
@@ -4,6 +4,38 @@ import subprocess
 import sys
 
 
+def getNextLine(lines, curr_i):
+    curr_i += 1
+    while curr_i < len(lines) and ("#" in lines[curr_i] or len(lines[curr_i]) == 0):
+        curr_i += 1
+    return curr_i
+
+
+def add_https_to_url(url):
+    if not url.startswith(("http://", "https://")):
+        url = "https://" + url
+    return url
+
+
+def getData(body):
+    data = {}
+    i = 0
+    line_i = 0
+    lines = body.split("\n")
+
+    line_i = getNextLine(lines, line_i)
+    data["company_name"] = lines[line_i]
+    line_i = getNextLine(lines, line_i)
+    data["title"] = lines[line_i]
+    line_i = getNextLine(lines, line_i)
+    data["url"] = add_https_to_url(lines[line_i].strip())
+    line_i = getNextLine(lines, line_i)
+    data["location"] = [line.strip() for line in lines[line_i].split(";")]
+    line_i = getNextLine(lines, line_i)
+    data["terms"] = [line.strip() for line in lines[line_i].split(",")]
+    return data
+
+
 def main():
     # Read the file path from the command-line arguments
     event_file_path = sys.argv[1]
@@ -24,8 +56,10 @@ def main():
     # if line.find("#") == -1:
     #     values.append(line.strip().lower())
 
+    data = getData(issue_body)
+
     with open("listings.json", "w") as f:
-        f.write(json.dumps(event_data, indent=4))
+        f.write(json.dumps(data, indent=4))
 
     print("::set-output name=new_internship::true")
 
