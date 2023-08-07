7e8eabed9b8b0920f3fa60d52dabace498fbfc44
Matt Reed <matthewreed02@gmail.com>
2023-08-07 12:52:32 -0400

    fix update_readmes

--- .github/scripts/update_readmes.py
+++ .github/scripts/update_readmes.py
@@ -9,31 +9,6 @@ def main():
 
     listings = util.getListingsFromJSON()
 
-    sponsorship_pattern = r'\(.*?sponsorship.*?\)'
-    citizen_pattern = r'\(.*?citizen.*?\)'
-    for listing in listings:
-        orig_title = listing["title"]
-        new_title = re.sub(sponsorship_pattern, '', listing["title"], flags=re.IGNORECASE)
-        no_sponsorship = orig_title != new_title
-        other = no_sponsorship and "f1" in orig_title.lower()
-        orig_title2 = new_title
-        new_title = re.sub(citizen_pattern, '', orig_title2, flags=re.IGNORECASE)
-        citizenship = new_title != orig_title2
-        listing["title"] = new_title
-        if other:
-            listing["sponsorship"] = "Other"
-        elif no_sponsorship:
-            listing["sponsorship"] = "Does Not Offer Sponsorship"
-        elif citizenship:
-            listing["sponsorship"] = "U.S. Citizenship is Required"
-        else:
-            listing["sponsorship"] = "Offers Sponsorship"
-
-    with open(".github/scripts/listings.json", "w") as f:
-        f.write(json.dumps(listings, indent=4))
-    return
-    
-
     util.checkSchema(listings)
     util.sortListings(listings)
 
